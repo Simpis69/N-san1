197ecfa014660f0fa552fa5989c39d2b91ec9888
Leonard wegmann simon <leonard.wegmannsimon@elev.ga.ntig.se>
2024-11-12 10:21:22 +0100

    ioisndf

---
+++ If-Sats/Program.cs
@@ -0,0 +1,36 @@
+﻿// #1
+// if(6>3)
+// {
+//     Console.WriteLine("Hello world");
+// }
+// Console.ReadLine();
+
+// #2
+// Console.WriteLine("Tjaboba vad är ditt användarnamn?");
+// string svar = Console.ReadLine().ToLower();
+
+// if (svar == "noname")
+// {
+//     Console.WriteLine("Correct username");
+// }
+// else
+// {
+//     Console.WriteLine("Incorrect username");
+// }
+
+// Console.WriteLine("what is your password");
+// string pass = Console.ReadLine().ToLower();
+
+// if(pass == "12345")
+// {
+//     Console.WriteLine("Welcome!");
+// }
+// else
+// {
+//     Console.WriteLine("wrong password");
+// }
+
+// #3
+
+
+Console.ReadLine();
