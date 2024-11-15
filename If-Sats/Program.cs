9bd6e191012437bd4b32f408f7f51ce05a845356
Leonard wegmann simon <leonard.wegmannsimon@elev.ga.ntig.se>
2024-11-15 13:29:31 +0100

    .iusbn

--- If-Sats/Program.cs
+++ If-Sats/Program.cs
@@ -32,5 +32,81 @@
 
 // #3
 
+// for(int i = 0 ; i < 32; i ++ )
+// {
+//     Console.WriteLine("Hello world");
+// }
+
+// #4
+
+// while (true)
+// {
+// Console.WriteLine("what is your password");
+// string pass = Console.ReadLine().ToLower();
+
+// if(pass == "12345")
+// {
+//     Console.WriteLine("Welcome!");
+//     break;
+// }
+// else
+// {
+//     Console.WriteLine("wrong password");
+// }
+
+// Console.ReadLine();
+// }
+
+// #5
+
+// for (int i = 0; i < 5; i++)
+// {
+//     Console.WriteLine("skriv ett nummer från 1-10");
+//     string tal = Console.ReadLine();
+//     int.TryParse(tal, out int tal2);
+
+//     if(tal2 > 5)
+//     {
+//         Console.WriteLine("högre än 5");
+//     }
+//     else
+//     {
+//         Console.WriteLine("lägre än 5");
+//     }
+//     else if (tal2 == 5)
+//     {
+//         Console.WriteLine("fem");
+//     }
+
+// }
+
+// #6
+
+// while (true)
+//         {
+//             Console.WriteLine("skriv vad du vill");
+//             string skriv = Console.ReadLine();
+//             if(int.TryParse(skriv, out int prat))
+//             {
+//                 Console.WriteLine("detta kan jag konvertera");
+//                 break;
+//             }
+//             else
+//             {
+//                 Console.WriteLine("Ge mig en utmaning");
+//             }
+
+
+//             Console.ReadLine();
+//         }
+
+// #7
+
+while(true)
+{
+    Console.WriteLine("gissa ett nummer mellan 1-100");
+}
+
+
 
 Console.ReadLine();
