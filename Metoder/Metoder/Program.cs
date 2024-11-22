4e2681ee7080572671c45a96e31c2fb8480e3148
Leonard wegmann simon <leonard.wegmannsimon@elev.ga.ntig.se>
2024-11-22 13:26:37 +0100

    onjsnef

---
+++ Metoder/Metoder/Program.cs
@@ -0,0 +1,32 @@
+﻿// static void hello32()
+// {
+//     for (int i = 0; i < 32; i++)
+//     {
+//         Console.WriteLine("hellowrodl");
+//     }
+// }
+
+// hello32();
+
+static void kvadrat(int square)
+{
+Console.WriteLine($"{square * square}");
+}
+
+kvadrat(5);
+
+static void multi(float one, float two)
+{
+    Console.WriteLine($"{one * two}");
+}
+
+multi(4, 5);
+
+static void RightTriangleArea(float bas, float hojd)
+{
+    Console.WriteLine($"{bas * hojd}");
+}
+
+multi(4,6);
+
+Console.ReadLine();
