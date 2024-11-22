a89d61c2f63e6f5a615a12f17206f8dd857030a2
Leonard wegmann simon <leonard.wegmannsimon@elev.ga.ntig.se>
2024-11-22 12:38:57 +0100

    ijsrbg

--- Listor&Arrayer/list&array/Program.cs
+++ Listor&Arrayer/list&array/Program.cs
@@ -1,1 +1,40 @@
-﻿
+﻿string [] leksaker = {"bil", "plan", "svärd", "bubblor", "spiderman"};
+
+string [] chompi = {"sverre", "Skibidi Leo", "Raschmuss", "David", "Micke"};
+
+int [] värden = {3, 5, 7, 9, 1};
+
+
+for (int i = 0; i < chompi.Length; i++)
+{
+Console.WriteLine($"{chompi[i]} ger {leksaker[i]} betyget {värden[i]}");
+}
+
+List<string> cities = new List<string>();
+
+Console.WriteLine("skriv en valfri stad!");
+Console.WriteLine("skriv exit för att stänga av");
+while(true)
+{
+    string city = Console.ReadLine();
+    
+    if(city.ToLower() == "exit")
+    {
+        break;
+    }
+    else
+    {
+        cities.Add(city);
+    }
+}
+foreach (string stad in cities)
+{
+    Console.WriteLine(stad);
+}
+
+
+// foreach (string leksak in leksaker)
+// {
+//     Console.WriteLine(leksak);
+// }
+Console.ReadLine();
